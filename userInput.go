39a6fe26a089ff1fb4b23329ba6fda0f25f54d45
haokaili <>
2021-05-06 16:37:16 +0000

    revise4

--- userInput.go
+++ userInput.go
@@ -11,16 +11,17 @@ func main() {
 	// This function does addition
 	var streetName string
 	var streetNumber int
-  
+
   // input
 	fmt.Println("This program gets a user's street number and name.")
 	fmt.Println()
 	fmt.Print("Enter your street Number: ")
 	fmt.Scanln(&streetNumber)
-  fmt.Print("Enter your street Name: ")
+	fmt.Print("Enter your street Name: ")
 	fmt.Scanln(&streetName)
-	
+
   // output
-	fmt.Println("Your street address is: ", streetNumber, " ", streetName, ".")
+	fmt.Println("Your street address is: ", streetNumber, " ", 
+  streetName, ".")
 	fmt.Println("\nDone.")
 }
